5aabc395d70e933d353fdddf1b3f5d570c90fb97
gvakhilesh <gvakhilesh.is21@rvce.edu.in>
2025-02-26 22:19:33 +0530

    first commit

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,4 +1,6 @@
-export const backendUrl = "http://localhost:8000";
+//export const backendUrl = "http://localhost:8000";
 // export const backendUrl = "http://3.108.54.68:8000";
 //export const backendUrl = "https://mind-meld-react.onrender.com";
+export const backendUrl = "https://mern-backend-new-i3ta.onrender.com";
+
 
